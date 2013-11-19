62d4fbe52aa2975fb6dd05749ea6e4f90db74f5c
zhzenghui <zhzenghui@gmail.com>
2013-11-20 01:50:12 +0800

    mongoid blog

---
+++ app/models/article.rb
@@ -0,0 +1,12 @@
+class Article
+  include Mongoid::Document
+  include Mongoid::MultiParameterAttributes
+  
+  field :name, type: String
+  field :content, type: String
+  field :published_on, :type => Date
+  
+  validates_presence_of :name
+  embeds_many :comments
+  
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ZhSite::Application.config.secret_key_base = 'f989a0e70ff29be0f3873a050513da8cda4b56080355e019a62a692cc9ba945ec48f6b49ed7b6b7a159472a494b5d0635d8221ffd4e9a19944567aa8694f5449'
